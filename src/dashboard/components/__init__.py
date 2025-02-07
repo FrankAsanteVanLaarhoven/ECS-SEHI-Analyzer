8fb256d8dff171661392e7a78c493281b06ae459
FAVL <frank@lacoupefutur.com>
2025-02-07 03:40:29 +0000

    Delete src/dashboard directory

--- src/dashboard/components/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Dashboard components initialization."""
-from .navigation import render_navigation
-
-__all__ = ['render_navigation'] 
--- src/dashboard/pages/model_playground.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from utils.model_manager import ModelManager
-
-def render_model_playground(dashboard):
-    """Render model playground page."""
-    st.title("Model Playground")
-    # ... (model playground implementation) 
